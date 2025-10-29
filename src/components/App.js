5af568829940cd9c8cae7bbad6d0f6acf0d5b3f5
nithinbairu <nithinbairu08@gmail.com>
2025-10-29 10:31:20 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-        <Tooltip text="This is a tooltip" >
-          <button>Hover over me</button>
-        </Tooltip>
-        <br></br>
-        <Tooltip text="This is another tooltip" >
-          <button>Hover over me to see another tooltip</button>
-        </Tooltip>
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <Tooltip text="Tooltip for heading">
+        <h2>Hover over this heading</h2>
+      </Tooltip>
 
-export default App
+      <Tooltip text="Tooltip for paragraph">
+        <p>Hover over this paragraph</p>
+      </Tooltip>
+
+      <Tooltip text="Tooltip for another heading">
+        <h2>Hover over another heading</h2>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default App;
